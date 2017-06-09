e7b8a5fb53ee4dd542a2b8ac40d176f27794b42b
Samuel Bernheim <sambernheim@gmail.com>
2017-06-08 22:21:24 -0400

    Scroll to top on state change

--- browser/js/app.js
+++ browser/js/app.js
@@ -10,3 +10,10 @@ app.config(function($locationProvider, $urlRouterProvider) {
 	// If no valid state is found, redirect the user to a 404 page
 	$urlRouterProvider.otherwise('/404');
 });
+
+app.run(function ($rootScope) {
+	// This brings the window to the top of the page after a state change --> Otherwise the page stays the same distance from the top of the screen as it was on the previous state.
+	$rootScope.$on('$stateChangeSuccess', function() {
+		document.body.scrollTop = document.documentElement.scrollTop = 0;
+	});
+});
--- server/public/main.js
+++ server/public/main.js
@@ -12,6 +12,13 @@ app.config(function ($locationProvider, $urlRouterProvider) {
 	$urlRouterProvider.otherwise('/404');
 });
 
+app.run(function ($rootScope) {
+	// This brings the window to the top of the page after a state change --> Otherwise the page stays the same distance from the top of the screen as it was on the previous state.
+	$rootScope.$on('$stateChangeSuccess', function () {
+		document.body.scrollTop = document.documentElement.scrollTop = 0;
+	});
+});
+
 'use strict';
 
 // Register a state called timer

875a3ebd06899f64716c33ea3aecc6dc2e1d579b
sbernheim <samuel.bernheim@mlb.com>
2017-06-09 16:00:09 -0400

    Add footer

---
+++ browser/js/footer/footer.js
@@ -0,0 +1,11 @@
+app.directive('footer', function($rootScope, $state) {
+
+	return {
+		restrict: 'E',
+		scope: {},
+		templateUrl: 'js/footer/footer.template.min.html',
+
+		link: function (scope) {
+		}
+	};
+});
--- server/public/main.js
+++ server/public/main.js
@@ -59,6 +59,17 @@ app.config(function ($stateProvider) {
 	});
 });
 
+app.directive('footer', function ($rootScope, $state) {
+
+	return {
+		restrict: 'E',
+		scope: {},
+		templateUrl: 'js/footer/footer.template.min.html',
+
+		link: function (scope) {}
+	};
+});
+
 'use strict';
 
 app.controller('HomeCtrl', function ($scope) {});

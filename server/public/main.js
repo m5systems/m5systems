6ee5ec44761fe9a435ffdaa168569a0f71e77cf3
Samuel Bernheim <sambernheim@gmail.com>
2017-06-11 19:32:19 -0400

    Fix testimonials section

--- server/public/main.js
+++ server/public/main.js
@@ -127,20 +127,6 @@ app.config(function ($stateProvider) {
 
 'use strict';
 
-app.controller('NetworkAndDatacenterVirtualizationCtrl', function ($scope) {});
-
-'use strict';
-
-app.config(function ($stateProvider) {
-	$stateProvider.state('network-and-datacenter-virtualization', {
-		url: '/solutions-and-services/network-and-datacenter-virtualization',
-		templateUrl: 'js/solutions-and-services/network-and-datacenter-virtualization/network-and-datacenter-virtualization.template.min.html',
-		controller: 'NetworkAndDatacenterVirtualizationCtrl'
-	});
-});
-
-'use strict';
-
 app.controller('CloudConsultingCtrl', function ($scope) {});
 
 'use strict';
@@ -155,15 +141,15 @@ app.config(function ($stateProvider) {
 
 'use strict';
 
-app.controller('NetworkSecurityCtrl', function ($scope) {});
+app.controller('NetworkAndDatacenterVirtualizationCtrl', function ($scope) {});
 
 'use strict';
 
 app.config(function ($stateProvider) {
-	$stateProvider.state('network-security', {
-		url: '/solutions-and-services/network-security',
-		templateUrl: 'js/solutions-and-services/network-security/network-security.template.min.html',
-		controller: 'NetworkSecurityCtrl'
+	$stateProvider.state('network-and-datacenter-virtualization', {
+		url: '/solutions-and-services/network-and-datacenter-virtualization',
+		templateUrl: 'js/solutions-and-services/network-and-datacenter-virtualization/network-and-datacenter-virtualization.template.min.html',
+		controller: 'NetworkAndDatacenterVirtualizationCtrl'
 	});
 });
 
@@ -178,6 +164,20 @@ app.config(function ($stateProvider) {
 		url: '/solutions-and-services/network-infrastructure',
 		templateUrl: 'js/solutions-and-services/network-infrastructure/network-infrastructure.template.min.html',
 		controller: 'NetworkInfrastructureCtrl'
+	});
+});
+
+'use strict';
+
+app.controller('NetworkSecurityCtrl', function ($scope) {});
+
+'use strict';
+
+app.config(function ($stateProvider) {
+	$stateProvider.state('network-security', {
+		url: '/solutions-and-services/network-security',
+		templateUrl: 'js/solutions-and-services/network-security/network-security.template.min.html',
+		controller: 'NetworkSecurityCtrl'
 	});
 });
 

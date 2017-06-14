1c03ed1259c917abc483b10f0797a1498abc588c
Samuel Bernheim <sambernheim@gmail.com>
2017-06-13 21:53:52 -0400

    Remove testimonials from home page

--- server/public/main.js
+++ server/public/main.js
@@ -147,20 +147,6 @@ app.config(function ($stateProvider) {
 
 'use strict';
 
-app.controller('NetworkAndDatacenterVirtualizationCtrl', function ($scope) {});
-
-'use strict';
-
-app.config(function ($stateProvider) {
-	$stateProvider.state('network-and-datacenter-virtualization', {
-		url: '/solutions-and-services/network-and-datacenter-virtualization',
-		templateUrl: 'js/solutions-and-services/network-and-datacenter-virtualization/network-and-datacenter-virtualization.template.min.html',
-		controller: 'NetworkAndDatacenterVirtualizationCtrl'
-	});
-});
-
-'use strict';
-
 app.controller('NetworkInfrastructureCtrl', function ($scope) {});
 
 'use strict';
@@ -170,6 +156,20 @@ app.config(function ($stateProvider) {
 		url: '/solutions-and-services/network-infrastructure',
 		templateUrl: 'js/solutions-and-services/network-infrastructure/network-infrastructure.template.min.html',
 		controller: 'NetworkInfrastructureCtrl'
+	});
+});
+
+'use strict';
+
+app.controller('NetworkAndDatacenterVirtualizationCtrl', function ($scope) {});
+
+'use strict';
+
+app.config(function ($stateProvider) {
+	$stateProvider.state('network-and-datacenter-virtualization', {
+		url: '/solutions-and-services/network-and-datacenter-virtualization',
+		templateUrl: 'js/solutions-and-services/network-and-datacenter-virtualization/network-and-datacenter-virtualization.template.min.html',
+		controller: 'NetworkAndDatacenterVirtualizationCtrl'
 	});
 });
 

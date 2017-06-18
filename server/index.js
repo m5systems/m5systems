9a7cd01220625108fe70e74f3273af5934afebac
Samuel Bernheim <sambernheim@gmail.com>
2017-06-18 12:00:38 -0400

    Change cache time to 2 weeks

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(compression());
 
 const port = process.env.PORT || 1337;
-const cacheTime = 43200000; // 12 hours
+const cacheTime = 1209600000; // 2 weeks
 
 // folder to serve public files --> css, img, etc
 // Folders inside public and the files inside them are also available --> Works recursively

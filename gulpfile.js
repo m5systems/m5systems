841dc82c6fe7a963902390fda56c9cd9549fbfd9
sbernheim <samuel.bernheim@mlb.com>
2017-06-19 11:08:52 -0400

    Comment out using purifycss for mobile navbar active feature

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ const livereload = require('gulp-livereload');
 gulp.task('buildCSSProduction', () => {
     return gulp.src('./browser/scss/index.scss')
         .pipe(sass().on('error', sass.logError)) // compile the sass file to a css file
-        .pipe(purify(['./browser/js/**/*.min.html']))
+        //.pipe(purify(['./browser/js/**/*.min.html']))
         .pipe(postcss([autoprefixer({browsers: ['> 1%']})]))
         .pipe(cleanCSS()) // minify the css file
         .pipe(gulp.dest('./server/public/')) // write the css file to ./server

2dcc183c8ceec7d44e20431af3369f8f22628d8a
Samuel Bernheim <sambernheim@gmail.com>
2017-06-26 19:47:12 -0400

    Require fields on the contact-us form

--- server/public/main.js
+++ server/public/main.js
@@ -169,20 +169,6 @@ app.config(function ($stateProvider) {
 
 'use strict';
 
-app.controller('NetworkAndDatacenterVirtualizationCtrl', function ($scope) {});
-
-'use strict';
-
-app.config(function ($stateProvider) {
-	$stateProvider.state('network-and-datacenter-virtualization', {
-		url: '/solutions-and-services/network-and-datacenter-virtualization',
-		templateUrl: 'js/solutions-and-services/network-and-datacenter-virtualization/network-and-datacenter-virtualization.template.min.html',
-		controller: 'NetworkAndDatacenterVirtualizationCtrl'
-	});
-});
-
-'use strict';
-
 app.controller('NetworkInfrastructureCtrl', function ($scope) {});
 
 'use strict';
@@ -192,6 +178,20 @@ app.config(function ($stateProvider) {
 		url: '/solutions-and-services/network-infrastructure',
 		templateUrl: 'js/solutions-and-services/network-infrastructure/network-infrastructure.template.min.html',
 		controller: 'NetworkInfrastructureCtrl'
+	});
+});
+
+'use strict';
+
+app.controller('NetworkAndDatacenterVirtualizationCtrl', function ($scope) {});
+
+'use strict';
+
+app.config(function ($stateProvider) {
+	$stateProvider.state('network-and-datacenter-virtualization', {
+		url: '/solutions-and-services/network-and-datacenter-virtualization',
+		templateUrl: 'js/solutions-and-services/network-and-datacenter-virtualization/network-and-datacenter-virtualization.template.min.html',
+		controller: 'NetworkAndDatacenterVirtualizationCtrl'
 	});
 });
 

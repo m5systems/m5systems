be457e4fd94403566468a6c2955dbf5867b59dfb
Samuel Bernheim <sambernheim@gmail.com>
2017-07-15 13:18:41 -0400

    Make homepage background photo random

--- browser/js/home/home.controller.js
+++ browser/js/home/home.controller.js
@@ -2,4 +2,13 @@
 
 app.controller('HomeCtrl', function($scope) {
 
+	$scope.chooseImage = function() {
+		let randomNum = Math.floor(Math.random() * 6);
+		let arrayOfImageUrls = ['background-1.jpg', 'background-2.jpg', 'background-3.jpg', 'background-4.jpg', 'background-5.jpg', 'background-6.jpg', 'background-7.jpg'];
+		let background = document.querySelector('.home__header');
+
+		background.style.backgroundImage = `url(images/home-background-photos/${arrayOfImageUrls[randomNum]})`;
+	};
+
+	$scope.chooseImage();
 });
